034ac1864e89d98ce78d6ba2d892ee21602862e1
DerFalscheHeld <simon@raspberrypi>
2023-01-07 17:59:55 +0100

    added installscrip (not working only echos the comand)

---
+++ install.bash
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [[ $UID != 0 ]] ; then
+  echo -e "\n\033[31mError : \033[33mYou are not root!\n"
+  exit
+fi
+
+mkdir -p '/usr/local/etc/backup/temp'
+path='/usr/local/etc/backup/temp/gitClonePath.temp'
+touch $path
+count=1
+while : ; do
+  pathPart=`echo $0 | cut -d'/' -f $count`
+  if [[ $count -eq 1 ]] ; then
+    echo -n $pathPart >> $path
+  else
+    if [[ "`echo $0 | cut -d'/' -f $(($count+1))`" = "" ]] ; then
+      echo -n /backup.bash >> $path
+      break
+    else
+      echo -n /$pathPart >> $path
+    fi
+  fi
+  count=$(($count+1))
+done
+echo "cp `cat $path` /usr/local/bin/backup"
+rm $path
