613c4a18e01bc0a22816f6855756cc91c7d397e8
root <root@raspberrypi>
2023-01-08 23:00:32 +0100

    loading bar addet to install skript

--- install.bash
+++ install.bash
@@ -1,14 +1,43 @@
 #!/bin/bash
 
+countLoad=1
+length=${COLUMNS:-$(tput cols)}
+length=$(($length-1))
+
+function load {
+  prozent=$(($length*$1/100))
+  echo -e -n "\033[32m["
+  while : ; do
+    echo -e -n "\033[32m\033[7m#\033[0m"
+    countLoad=$(($countLoad+1))
+    if [[ $countLoad -eq $prozent ]] ; then
+      while : ; do
+       countLoad=$(($countLoad+1))
+       if [[ $countLoad -gt $length ]] ; then
+         break
+       fi
+       echo -e -n "\033[33m-"
+      done
+      echo -e -n "\033[32m]"
+      echo -e -n "\r"
+      countLoad=1
+      break
+    fi
+  done
+}
+
 if [[ $UID != 0 ]] ; then
   echo -e "\n\033[31mError : \033[33mYou are not root!\n"
   exit
 fi
 
 mkdir -p '/usr/local/bin'
+load 17
 mkdir -p '/usr/local/etc/backup/temp'
+load 34
 path='/usr/local/etc/backup/temp/gitClonePath.temp'
 touch $path
+load 51
 count=1
 while : ; do
   pathPart=`echo $0 | cut -d'/' -f $count`
@@ -24,5 +53,9 @@ while : ; do
   fi
   count=$(($count+1))
 done
+load 68
 cp `cat $path` /usr/local/bin/backup
+load 85
 rm $path
+load 100
+echo -e "\033[32m[Successfully installed"
